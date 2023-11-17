f9e19ff7534d5cdb33ecaa24be382d1683059b58
Elizafahr <124281054+Elizafahr@users.noreply.github.com>
2023-11-18 01:17:28 +0500

    Delete PHP и MySQL directory

--- PHP и MySQL/1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root");
-if($conn ->connect_error){
-    die("Ошибка". $conn ->connect_error);
-}
-echo"Подключение успешно установлено";
-$conn ->close();
-?>
